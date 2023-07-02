8a53032a41376ab54783e43dd320a7d6a2194da7
Akshay Kadam <ar1pperk@gmail.com>
2023-07-02 10:44:01 +0530

    resolved conflicts

--- src/pages/categories/list.tsx
+++
@@ -1,6 +0,0 @@
-import { IResourceComponentsProps } from "@refinedev/core";
-import { ChakraUIListInferencer } from "@refinedev/inferencer/chakra-ui";
-
-export const CategoryList: React.FC<IResourceComponentsProps> = () => {
-  return <ChakraUIListInferencer />;
-};
---
+++ src/pages/stories/index.ts
@@ -0,0 +1,4 @@
+export { StoryList } from "./list";
+export { StoryCreate } from "./create";
+export { StoryEdit } from "./edit";
+export { StoryShow } from "./show";
