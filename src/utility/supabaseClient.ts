87fe7a229e9c5e957a0cad7f11a63a0358c655dd
Akshay Kadam <ar1pperk@gmail.com>
2023-07-08 18:06:46 +0530

    resolved conflicts

--- src/utility/supabaseClient.ts
+++ src/utility/supabaseClient.ts
@@ -1,8 +1,9 @@
 import { createClient } from "@refinedev/supabase";
 
-const SUPABASE_URL = "https://xpxjwfqmxvqiyijdtkkt.supabase.co";
-const SUPABASE_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhweGp3ZnFteHZxaXlpamR0a2t0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODc5NjAzMjksImV4cCI6MjAwMzUzNjMyOX0.7JpI6AnXnPCR5NZyEKcfAILXkWDcUKkNfcZfza-hQH8";
+//read from .env file
+const SUPABASE_URL = "https://xpxjwfqmxvqiyijdtkkt.supabase.co"
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhweGp3ZnFteHZxaXlpamR0a2t0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODc5NjAzMjksImV4cCI6MjAwMzUzNjMyOX0.7JpI6AnXnPCR5NZyEKcfAILXkWDcUKkNfcZfza-hQH8"
+
 
 export const supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY, {
   db: {
