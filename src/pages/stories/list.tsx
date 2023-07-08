3bbbb006f998eebdc4db24f4b49de3afd3d9cc54
Akshay Kadam <ar1pperk@gmail.com>
2023-07-08 18:07:19 +0530

    resolved conflicts

--- src/pages/stories/list.tsx
+++ src/pages/stories/list.tsx
@@ -1,12 +1,12 @@
 import { Button, useDisclosure } from "@chakra-ui/react";
 import { AddItem } from "../../components/forms/add-item";
-// only to test ui 
+// only to test ui
 export const StoryList: React.FC = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure()
-    return (
-      <>
-       <Button onClick={onOpen}>Add itinerary item</Button>
-       <AddItem isOpen={isOpen} onClose={onClose} />
-      </>
-    );
-  };
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <Button onClick={onOpen}>Add itinerary item</Button>
+      <AddItem isOpen={isOpen} onClose={onClose} />
+    </>
+  );
+};
