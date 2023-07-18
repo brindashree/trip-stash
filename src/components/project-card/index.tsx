700fe7d441f3c967ceca46cd1cb04d9d8382a10c
Akshay Kadam <ar1pperk@gmail.com>
2023-07-18 23:16:27 +0530

    inprogress

--- src/components/project-card/index.tsx
+++ src/components/project-card/index.tsx
@@ -52,7 +52,8 @@ export const ProjectCard: React.FC<IProject> = (props) => {
 
   return (
     <Card
-      direction={{ base: "column", sm: "row" }}
+      mx="4"
+      direction={{ base: "column", lg: "row" }}
       overflow="hidden"
       variant="filled"
       my={8}
@@ -62,7 +63,7 @@ export const ProjectCard: React.FC<IProject> = (props) => {
         background={is_private ? COLORS.warning500 : COLORS.primaryColor}
         color={"white"}
         position={"absolute"}
-        top={"50%"}
+        top={{ base: 8, lg: "50%" }}
         right={4}
       >
         <TagLabel
@@ -75,13 +76,12 @@ export const ProjectCard: React.FC<IProject> = (props) => {
       </Tag>
       <Image
         objectFit="cover"
-        // maxW={{ base: "100%", sm: "200px" }}
         src="https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60"
         alt="Caffe Latte"
         pt={4}
         pb={4}
         pl={4}
-        htmlWidth="240px"
+        width={{ base: "100%", lg: "30%" }}
         borderRadius="3xl"
       />
       <CardBody>
@@ -135,6 +135,7 @@ export const ProjectCard: React.FC<IProject> = (props) => {
               size="lg"
               colorScheme={getProjectStatusColor(status)}
               width={"fit-content"}
+              mr={2}
             >
               <TagLabel>{status}</TagLabel>
             </Tag>
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,105 +1,63 @@
-import React from "react";
-import HeroImage from "../../assets/hero.png";
-import RightImage from "../../assets/right.png";
-import LeftImage from "../../assets/left.png";
-import { Image, Text, Flex, Spacer, Heading, Button } from "@chakra-ui/react";
-import { useNavigation } from "@refinedev/core";
-import { useIsAuthenticated } from "@refinedev/core";
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  Flex,
+  Heading,
+  Spacer,
+  Text,
+} from "@chakra-ui/react";
+import { useGetIdentity, useNavigation } from "@refinedev/core";
+import { IUser } from "../../utility/interface";
+import { IconPlus } from "@tabler/icons";
+import { COLORS } from "../../utility/colors";
 
 export function Home() {
   const { push } = useNavigation();
-  const { data } = useIsAuthenticated();
-
-  const handleRouteToLogin = () => {
-    if (data?.authenticated) {
-      push("/projects");
-      return;
-    }
-
-    push("/login");
-  };
-
+  const { data: user } = useGetIdentity<IUser>();
   return (
-    <div style={{ padding: " 8px", width: "80%", margin: "0 auto" }}>
-      <Spacer h="48px" />
-      <Flex direction="column" alignItems={"center"}>
-        <Heading textAlign={"center"} as="h1" size="xl" mb="4">
-          Collaborative Travel Made Easy
+    <div>
+      <div>
+        <Heading as="h2" size="xl" mb="4">
+          Welcome back, {user?.email}
         </Heading>
-        <Text textAlign={"center"}>
-          Turn your collective travel inspirations into unforgettable journey
-          with TripStash. Plan Together, Experience Together.
-        </Text>
-        <Button
-          margin={"auto"}
-          mt="8"
-          colorScheme="blue"
-          variant="solid"
-          size="md"
-          onClick={handleRouteToLogin}
-        >
-          {data?.authenticated ? "Go to projects" : "Join us now"}
-        </Button>
-        <Image
-          src={HeroImage}
-          alt="Hero Image"
-          width="100%"
-          height="auto"
-          mt="8"
-        />
-      </Flex>
+        <Flex gap="4">
+          <Card>
+            <CardHeader>
+              <Text>Your active projects</Text>
+            </CardHeader>
+            <CardBody>
+              <Heading as="h1" size="2xl">
+                1
+              </Heading>
+            </CardBody>
+          </Card>
 
-      <Flex
-        direction={[
-          "column-reverse",
-          "column-reverse",
-          "column-reverse",
-          "row",
-        ]}
-        alignItems={"center"}
-        mt={16}
-        gap={"16%"}
-      >
-        <div>
-          <Heading as="h3" size="xl" mb="4" mt="12">
-            Collaborative Itinerary
-          </Heading>
-          <Text>
-            Make group travel planning a breeze. Create projects and invite
-            friends or family to collaboratively add, vote on, and finalize
-            itinerary items. Share ideas, keep track of bookings, and create
-            your perfect journey together with TripStash.
-          </Text>
-        </div>
-        <Image
-          src={RightImage}
-          alt="Hero Image"
-          width="100%"
-          height="auto"
-          mt="8"
-        />
-      </Flex>
-
-      <Flex
-        direction={["column", "column", "column", "row"]}
-        alignItems={"center"}
-        mt={16}
-        gap={"16%"}
-      >
-        <Image src={LeftImage} alt="Hero Image" width="100%" height="auto" />
-        <div>
-          <Heading as="h3" size="xl" mb="4" mt="12">
-            Multimedia Inspiration
-          </Heading>
-          <Text>
-            Travel planning starts with inspiration. With TripStash, you can
-            collect and organize all the images, videos, and articles that
-            inspire your wanderlust. Share your sources of inspiration within
-            your group and use them as a starting point for your itinerary. Turn
-            the dream into reality with a click.
-          </Text>
-        </div>
-      </Flex>
+          <Card
+            backgroundColor={COLORS.primaryColor}
+            color={"white"}
+            alignItems={"center"}
+            justifyContent={"center"}
+            padding={4}
+            flexWrap={"nowrap"}
+            flexDirection={"row"}
+            cursor={"pointer"}
+            onClick={() => push("/projects/create")}
+          >
+            <IconPlus />
+            Create a new project
+          </Card>
+        </Flex>
+      </div>
+      <Spacer height={100} />
+      <div>
+        <Heading as="h2" size="xl" mb="4">
+          Your Stash
+        </Heading>
+        <Heading as="h4" size="xs" mb="4" color={COLORS.greyNeutral500}>
+          View and manage all the projects you are a part of here
+        </Heading>
+      </div>
     </div>
   );
 }
