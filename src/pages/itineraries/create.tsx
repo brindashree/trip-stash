5637df6f40338a06dd94b32e255586e6861975fc
Akshay Kadam <ar1pperk@gmail.com>
2023-07-20 22:11:13 +0530

    fixed dates

--- src/pages/itineraries/create.tsx
+++ src/pages/itineraries/create.tsx
@@ -15,7 +15,7 @@ import {
   Heading,
   Flex,
   Textarea,
-  Text,
+  Box,
 } from "@chakra-ui/react";
 import { useForm } from "@refinedev/react-hook-form";
 import { ACTIVITIES, ITINERARY_STATUS } from "../../utility/constants";
@@ -57,8 +57,8 @@ export const ItineraryCreate: React.FC<IResourceComponentsProps> = () => {
       media_url: [values?.media_url],
       added_by: {
         id: user?.id,
-        email: user?.email
-      }
+        email: user?.email,
+      },
     }).then(() => navigate(`/${params?.projectId}/itinerary`));
   };
 
@@ -75,84 +75,86 @@ export const ItineraryCreate: React.FC<IResourceComponentsProps> = () => {
         </Heading>
       }
     >
-      <Flex gap={4} mt={8}>
-        <FormControl mb="3" isInvalid={!!(errors as any)?.title}>
-          <FormLabel>Title</FormLabel>
+      <Box py={4} px={8}>
+        <Flex gap={4} mt={8}>
+          <FormControl mb="3" isInvalid={!!(errors as any)?.title}>
+            <FormLabel>Title</FormLabel>
+            <Input
+              id="title"
+              type="text"
+              {...register("title", {
+                required: "This field is required",
+              })}
+            />
+            <FormErrorMessage>
+              {(errors as any)?.title?.message as string}
+            </FormErrorMessage>
+          </FormControl>
+
+          <FormControl mb="3" isInvalid={!!(errors as any)?.date}>
+            <FormLabel>Date</FormLabel>
+            <Input
+              id="date"
+              type="date"
+              min={itineraryMinStartDate}
+              max={itineraryMaxStartDate}
+              {...register("date", {
+                required: "This field is required",
+              })}
+            />
+          </FormControl>
+          <FormErrorMessage>
+            {(errors as any)?.date?.message as string}
+          </FormErrorMessage>
+        </Flex>
+
+        <FormControl mb="3" isInvalid={!!(errors as any)?.location}>
+          <FormLabel>Location</FormLabel>
           <Input
-            id="title"
+            id="location"
             type="text"
-            {...register("title", {
+            {...register("location", {
               required: "This field is required",
             })}
           />
           <FormErrorMessage>
-            {(errors as any)?.title?.message as string}
+            {(errors as any)?.location?.message as string}
           </FormErrorMessage>
         </FormControl>
 
-        <FormControl mb="3" isInvalid={!!(errors as any)?.date}>
-          <FormLabel>Date</FormLabel>
-          <Input
-            id="date"
-            type="date"
-            min={itineraryMinStartDate}
-            max={itineraryMaxStartDate}
-            {...register("date", {
+        <FormControl mb="3" isInvalid={!!(errors as any)?.media_url}>
+          <FormLabel>Media URL</FormLabel>
+          <Input id="media_url" type="text" {...register("media_url", {})} />
+          <FormErrorMessage>
+            {(errors as any)?.media_url?.message as string}
+          </FormErrorMessage>
+        </FormControl>
+        <FormControl mb="3" isInvalid={!!(errors as any)?.type_of_activity}>
+          <FormLabel>Type of Activity</FormLabel>
+          <Select
+            id="type_of_activity"
+            placeholder="Select activity"
+            {...register("type_of_activity", {
               required: "This field is required",
             })}
-          />
+          >
+            {ACTIVITIES?.map((option) => (
+              <option value={option.label} key={option.value}>
+                {option.label}
+              </option>
+            ))}
+          </Select>
+          <FormErrorMessage>
+            {(errors as any)?.type_of_activity?.message as string}
+          </FormErrorMessage>
         </FormControl>
-        <FormErrorMessage>
-          {(errors as any)?.date?.message as string}
-        </FormErrorMessage>
-      </Flex>
 
-      <FormControl mb="3" isInvalid={!!(errors as any)?.location}>
-        <FormLabel>Location</FormLabel>
-        <Input
-          id="location"
-          type="text"
-          {...register("location", {
-            required: "This field is required",
-          })}
-        />
-        <FormErrorMessage>
-          {(errors as any)?.location?.message as string}
-        </FormErrorMessage>
-      </FormControl>
-
-      <FormControl mb="3" isInvalid={!!(errors as any)?.media_url}>
-        <FormLabel>Media URL</FormLabel>
-        <Input id="media_url" type="text" {...register("media_url", {})} />
-        <FormErrorMessage>
-          {(errors as any)?.media_url?.message as string}
-        </FormErrorMessage>
-      </FormControl>
-      <FormControl mb="3" isInvalid={!!(errors as any)?.type_of_activity}>
-        <FormLabel>Type of Activity</FormLabel>
-        <Select
-          id="type_of_activity"
-          placeholder="Select activity"
-          {...register("type_of_activity", {
-            required: "This field is required",
-          })}
-        >
-          {ACTIVITIES?.map((option) => (
-            <option value={option.label} key={option.value}>
-              {option.label}
-            </option>
-          ))}
-        </Select>
-        <FormErrorMessage>
-          {(errors as any)?.type_of_activity?.message as string}
-        </FormErrorMessage>
-      </FormControl>
-
-      <FormControl mb="3" isInvalid={!!(errors as any)?.notes}>
-        <FormLabel>Notes</FormLabel>
-        <Textarea id="notes" size="sm" {...register("notes", {})} />
-        <FormErrorMessage>{`${errors.notes?.message}`}</FormErrorMessage>
-      </FormControl>
+        <FormControl mb="3" isInvalid={!!(errors as any)?.notes}>
+          <FormLabel>Notes</FormLabel>
+          <Textarea id="notes" size="sm" {...register("notes", {})} />
+          <FormErrorMessage>{`${errors.notes?.message}`}</FormErrorMessage>
+        </FormControl>
+      </Box>
     </Create>
   );
 };
